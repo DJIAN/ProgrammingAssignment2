54a8d36fedcb588f770b38a92c186665ca400a1c
DJIAN <yydjian@yahoo.fr>
2015-01-25 17:04:49 +0100

    C'est bon ...

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Coursera R Programming Week 3
+# F. DJIAN
+# 25 janvier 2015
 
-## Write a short comment describing this function
+# What this function does :
+#   Working with a matrix and cache its inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-  M_INV <- ???
-  set <- function (y)
+
+makeCacheMatrix <- function(MyMatrix = matrix()) {  
+
+  print("Debut function ...makeCacheMatrix")
+  inverse <- NULL
+    
+#  # Verify if this is a matrix :
+#  if (is.matrix(MyMatrix)) {
+#    print("Arg is a matrix ...: Good")
+#  }
+#  else {
+#    print("It's not a matrix ...")
+#    print("exiting ...")
+#	return (2);
+#  }
+    
+  # Set the apropriate env 
+  set <- function(y) {                
+    MyMatrix <<- y
+    inverse <<- NULL
+  }
   
+  get <- function() {
+    MyMatrix
+  }
 
+setinverse <- function(inv)
+{
+inverse <<- inv
 }
+## return the cached value of the inverse of the matrix.
+## returns NULL if the inverse has not yet be set, or the underlying
+## matrix has changed since the last invocation
+getinverse <- function()
+{
+inverse
+}
+## return a list of getter and setter methods as a result of object creation
+list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)
 
-
+}      
+  
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+	
+inv <- m$getinverse()
+if(!is.null(inv))
+{
+## Inverse already calculated, value not stale
+message("getting cached data")
+return(inv)
+}	
+
+} 
+
